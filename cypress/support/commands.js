49d2cac7cfca66382f5af45d3d8934ed9bbfaa3f
sabrinaschimidt <sabrinargschimidt@hotmail.com>
2024-11-13 16:20:52 -0400

    update

--- cypress/support/commands.js
+++
@@ -1,7 +0,0 @@
-Cypress.Commands.add('login', (username, password) => {
-    cy.visit('https://app.easyjur.com');
-    cy.get('#loginField').type(username);
-    cy.get('#passwordField').type(password);
-    cy.get('#loginButton').click();
-  });
-  
